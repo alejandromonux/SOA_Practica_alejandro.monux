7c210eb0c099093f880bbd8ddd3f62ede89a2cb6
alejandromonux <alejandro.monux@students.salleurl.edu>
2021-04-12 18:07:20 +0200

    Chore: main.c created for template purposes

---
+++ main.c
@@ -0,0 +1,11 @@
+int main(int argc, char const *argv[]) {
+
+  if (argc < 3){
+    write(1, "Not enough arguments provided. Current amount of arguments is: %d", argc);
+    return 0
+  }
+
+  
+
+  return 0;
+}
