9a4f627508d6dd8df183e8c901e4c907d4a58da1
alejandromonux <alejandro.monux@students.salleurl.edu>
2021-04-12 19:50:10 +0200

    Feat: Added filesystem check on main (EXT2 version)

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
+#include "./ext2/ext2_info.h"
+
 int main(int argc, char const *argv[]) {
 
   if (argc < 3){
-    write(1, "Not enough arguments provided. Current amount of arguments is: %d", argc);
-    return 0
+    printf("Not enough arguments provided. Current amount of arguments is: %d", argc);
+    return 0;
   }
 
-  
+  int fd = open(argv[2], 'r');
+
+  if(readFileTypeExt(fd) == 1){
+    printf("Sam va de puta madre socio galaxy\n");
+  }else{
+    printf("Sam no va tambien la verdá\n");
+  }
+
 
   return 0;
 }
