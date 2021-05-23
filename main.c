56468e7304b14801af977e696fa7597e98ba027d
Alejandro <alejandro.monux@students.salle.url.edu>
2021-05-23 19:07:18 +0200

    Chore: Main file verifications

--- main.c
+++ main.c
@@ -10,30 +10,33 @@
 int main(int argc, char const *argv[]) {
 
   if (argc < 3){
-    printf("Not enough arguments provided. Current amount of arguments is: %d", argc);
+    printf("No hi ha suficients arguments. El nombre actual d'arguments es: %d\n", argc);
     return 0;
   }
 
   int fd = open(argv[2], O_RDWR);
-
+  if (fd < 0) {
+    printf("Volum inexistent.\n");
+    return -1;
+  }
   if(readFileTypeExt(fd) == 1){
     printf("FILESYSTEM: EXT2\n\n");
     if(strcmp("/info", argv[1]) == 0){
       printExtInfo(fd);
     }else if (strcmp("/find", argv[1]) == 0) {
       if (argc < 4){
-        printf("No hi ha suficients arguments. El nombre actual d'arguments es: %d", argc);
+        printf("No hi ha suficients arguments. El nombre actual d'arguments es: %d\n", argc);
         return 0;
       }
       trobarArxiuExt(fd, argv[3],0,0);
     }else if (strcmp("/delete", argv[1]) == 0){
       if (argc < 4){
-        printf("No hi ha suficients arguments. El nombre actual d'arguments es: %d", argc);
+        printf("No hi ha suficients arguments. El nombre actual d'arguments es: %d\n", argc);
         return 0;
       }
       trobarArxiuExt(fd, argv[3],0,1);
     }else{
-        printf("Operació no suportada.");
+        printf("Operació no suportada.\n");
     }
   }else{
     if(isItFAT16(fd)){
@@ -42,18 +45,18 @@ int main(int argc, char const *argv[]) {
           printFAT16Info(fd);
         }else if (strcmp("/find", argv[1]) == 0) {
           if (argc < 4){
-            printf("No hi ha suficients arguments. El nombre actual d'arguments es: %d", argc);
+            printf("No hi ha suficients arguments. El nombre actual d'arguments es: %d\n", argc);
             return 0;
           }
           trobarArxiuFAT(fd, argv[3],0,0);
         }else if (strcmp("/delete", argv[1]) == 0){
           if (argc < 4){
-            printf("No hi ha suficients arguments. El nombre actual d'arguments es: %d", argc);
+            printf("No hi ha suficients arguments. El nombre actual d'arguments es: %d\n", argc);
             return 0;
           }
           trobarArxiuFAT(fd, argv[3],0,1);
         }else{
-          printf("Operació no suportada.");
+          printf("Operació no suportada.\n");
         }
 
     }else{
